936a7055d25e1b7dd11abca91eda4d7e64a83285
Yibo Cai <yibo.cai@arm.com>
2020-04-20 04:48:36 +0000

    scripts: Launch kubernetes test cluster with minikube
    Prepare kubernetes environment for end to end integration tests.
    - run "scripts/minikube.sh up" to launch kubernetes
    - run "scripts/minikube.sh clean" to cleanup
    
    Change-Id: I5160fcd3ab11ed15be467b74759ac55af515684f
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk-csi/+/3143
    Tested-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Maciej Szwed <maciej.szwed@intel.com>
    Reviewed-by: Xiaodong Liu <xiaodong.liu@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>

---
+++ scripts/minikube.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+KUBE_VERSION=${KUBE_VERSION:-v1.17.0}
+MINIKUBE_VERSION=${MINIKUBE_VERSION:-v1.7.3}
+MINIKUBE_DRIVER=${MINIKUBE_DRIVER:-none}
+MINIKUBE_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then
+	MINIKUBE_ARCH=arm64
+fi
+
+function install_minikube() {
+	if hash minikube 2> /dev/null; then
+		version=$(minikube version | awk '{print $3; exit;}')
+		if [[ "${version}" != "${MINIKUBE_VERSION}" ]]; then
+			echo "installed minikube doesn't match requested version ${MINIKUBE_VERSION}"
+			echo "please remove minikube ${version} first"
+			exit 1
+		fi
+		echo "minikube-${version} already installed"
+		return
+	fi
+
+	echo "=== downloading minikube-${MINIKUBE_VERSION}"
+	curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/"${MINIKUBE_VERSION}"/minikube-linux-"${MINIKUBE_ARCH}" && chmod +x /usr/local/bin/minikube
+}
+
+case "$1" in
+	up)
+		install_minikube
+		echo "=== starting minikube with kubeadm bootstrapper"
+		CHANGE_MINIKUBE_NONE_USER=true minikube start -b kubeadm --kubernetes-version="${KUBE_VERSION}" --vm-driver="${MINIKUBE_DRIVER}"
+		"/var/lib/minikube/binaries/${KUBE_VERSION}/kubectl" cluster-info
+		;;
+	down)
+		minikube stop
+		;;
+	clean)
+		minikube delete
+		;;
+	*)
+		echo "$0 [up|down|clean]"
+		;;
+esac
