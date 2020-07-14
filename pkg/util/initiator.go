5c9b761ebfda5db776a790cf160f2a5f9ffc8f6c
Yibo Cai <yibo.cai@arm.com>
2020-07-14 09:30:07 +0000

    pkg/util: Implement nvmf initiator interface
    Change-Id: I26443c8e45f279741136da1d818cc0098266314b
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>
    Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk-csi/+/3355
    Reviewed-by: Xiaodong Liu <xiaodong.liu@intel.com>
    Reviewed-by: Maciej Szwed <maciej.szwed@intel.com>
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Tomasz Zawadzki <tomasz.zawadzki@intel.com>
    Tested-by: Jim Harris <james.r.harris@intel.com>

---
+++ pkg/util/initiator.go
@@ -0,0 +1,149 @@
+/*
+Copyright 2020 The SPDK-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"k8s.io/klog"
+)
+
+// SpdkCsiInitiator defines interface for NVMeoF/iSCSI initiator
+//
+// - Connect initiates target connection and returns local block device filename
+//   e.g., /dev/disk/by-id/nvme-SPDK_Controller1_SPDK00000000000001
+// - Disconnect terminates target connection
+//
+// - Caller(node service) should serialize calls to same initiator
+// - Implementation should be idempotent to duplicated requests
+type SpdkCsiInitiator interface {
+	Connect() (string, error)
+	Disconnect() error
+}
+
+func NewSpdkCsiInitiator(volumeContext map[string]string) (SpdkCsiInitiator, error) {
+	targetType := strings.ToLower(volumeContext["targetType"])
+	switch targetType {
+	case "rdma", "tcp":
+		return &initiatorNVMf{
+			// see util/nvmf.go VolumeInfo()
+			targetType: volumeContext["targetType"],
+			targetAddr: volumeContext["targetAddr"],
+			targetPort: volumeContext["targetPort"],
+			nqn:        volumeContext["nqn"],
+			model:      volumeContext["model"],
+		}, nil
+	case "iscsi":
+		return nil, fmt.Errorf("iSCSI initiator not supported yet")
+	default:
+		return nil, fmt.Errorf("unknown initiator: %s", targetType)
+	}
+}
+
+// NVMf initiator implementation
+type initiatorNVMf struct {
+	targetType string
+	targetAddr string
+	targetPort string
+	nqn        string
+	model      string
+}
+
+func (nvmf *initiatorNVMf) Connect() (string, error) {
+	// nvme connect -t tcp -a 192.168.1.100 -s 4420 -n "nqn"
+	cmdLine := []string{"nvme", "connect", "-t", strings.ToLower(nvmf.targetType),
+		"-a", nvmf.targetAddr, "-s", nvmf.targetPort, "-n", nvmf.nqn}
+	err := execWithTimeout(cmdLine, 40)
+	if err != nil {
+		// go on checking device status in case caused by duplicated request
+		klog.Errorf("command %v failed: %s", cmdLine, err)
+	}
+
+	deviceGlob := fmt.Sprintf("/dev/disk/by-id/*%s*", nvmf.model)
+	devicePath, err := waitForDeviceReady(deviceGlob, 20)
+	if err != nil {
+		return "", err
+	}
+	return devicePath, nil
+}
+
+func (nvmf *initiatorNVMf) Disconnect() error {
+	// nvme disconnect -n "nqn"
+	cmdLine := []string{"nvme", "disconnect", "-n", nvmf.nqn}
+	err := execWithTimeout(cmdLine, 40)
+	if err != nil {
+		// go on checking device status in case caused by duplicate request
+		klog.Errorf("command %v failed: %s", cmdLine, err)
+	}
+
+	deviceGlob := fmt.Sprintf("/dev/disk/by-id/*%s*", nvmf.model)
+	return waitForDeviceGone(deviceGlob, 20)
+}
+
+// wait for device file comes up or timeout
+func waitForDeviceReady(deviceGlob string, seconds int) (string, error) {
+	for i := 0; i <= seconds; i++ {
+		time.Sleep(time.Second)
+		matches, err := filepath.Glob(deviceGlob)
+		if err != nil {
+			return "", err
+		}
+		// two symbol links under /dev/disk/by-id/ to same device
+		if len(matches) >= 1 {
+			return matches[0], nil
+		}
+	}
+	return "", fmt.Errorf("timed out waiting device ready: %s", deviceGlob)
+}
+
+// wait for device file gone or timeout
+func waitForDeviceGone(deviceGlob string, seconds int) error {
+	for i := 0; i <= seconds; i++ {
+		time.Sleep(time.Second)
+		matches, err := filepath.Glob(deviceGlob)
+		if err != nil {
+			return err
+		}
+		if len(matches) == 0 {
+			return nil
+		}
+	}
+	return fmt.Errorf("timed out waiting device gone: %s", deviceGlob)
+}
+
+// exec shell command with timeout(in seconds)
+func execWithTimeout(cmdLine []string, timeout int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+
+	klog.Infof("running command: %v", cmdLine)
+	cmd := exec.CommandContext(ctx, cmdLine[0], cmdLine[1:]...)
+	output, err := cmd.CombinedOutput()
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return fmt.Errorf("timed out")
+	}
+	if output != nil {
+		klog.Infof("command returned: %s", output)
+	}
+	return err
+}
---
+++ pkg/util/initiator_test.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2020 The SPDK-CSI Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// whitebox test of some functions in initiator.go
+package util
+
+import (
+	"testing"
+	"time"
+)
+
+func TestExecWithTimeoutPositive(t *testing.T) {
+	elapsed, err := runExecWithTimeout([]string{"true"}, 10)
+	if err != nil {
+		t.Fatal("should succeed")
+	}
+	if elapsed > 3 {
+		t.Fatal("timeout error")
+	}
+}
+
+func TestExecWithTimeoutNegative(t *testing.T) {
+	elapsed, err := runExecWithTimeout([]string{"false"}, 10)
+	if err == nil {
+		t.Fatal("should fail")
+	}
+	if elapsed > 3 {
+		t.Fatal("timeout error")
+	}
+}
+
+func TestExecWithTimeoutTimeout(t *testing.T) {
+	elapsed, err := runExecWithTimeout([]string{"sleep", "10"}, 1)
+	if err == nil {
+		t.Fatal("should fail")
+	}
+	if elapsed > 3 {
+		t.Fatal("timeout error")
+	}
+}
+
+func runExecWithTimeout(cmdLine []string, timeout int) (int, error) {
+	start := time.Now()
+	err := execWithTimeout(cmdLine, timeout)
+	elapsed := int(time.Since(start) / time.Second)
+	return elapsed, err
+}
