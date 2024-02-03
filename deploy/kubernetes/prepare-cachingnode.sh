56cc325bfb626e49f8e92df3d779f943e327dfb4
Manohar Reddy <b.manu199@gmail.com>
2024-02-03 22:35:22 +0100

    tested preapre-cachingnode.sh in eks

--- deploy/kubernetes/prepare-cachingnode.sh
+++ deploy/kubernetes/prepare-cachingnode.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-sudo vi /etc/sysctl.conf
-vm.nr_hugepages=2048
-sudo sysctl -p
-sudo sysctl -w vm.nr_hugepages=2048
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script requires sudo privileges. Please enter your password:"
+    exec sudo "$0" "$@" # This re-executes the script with sudo
+fi
+
+echo "======= setting huge pages ======="
+echo "vm.nr_hugepages=2048" >>/etc/sysctl.conf
+sysctl -p
 
 # confirm it by running
 cat /proc/meminfo | grep -i hug
 
-sudo mkdir /mnt/huge
-sudo mount -t hugetlbfs -o size=2G nodev /mnt/huge
-
-sudo vi /etc/fstab
-nodev /mnt/huge hugetlbfs size=2G 0 0
-
-kubectl taint nodes <node-name >tag=sbcache:NoSchedule
-
-# make sure that port 5000 is available
+echo "======= creating huge pages mount ======="
+mkdir /mnt/huge
+mount -t hugetlbfs -o size=2G nodev /mnt/huge
+echo "nodev /mnt/huge hugetlbfs size=2G 0 0" >>/etc/fstab
